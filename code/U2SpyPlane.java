9f6c41b4270c0b8f6c8ff965d6eb3e2dacf0a2e3
skyclarkchangcoco <123862183+megelclarkchangcoco@users.noreply.github.com>
2023-05-26 13:06:15 +0800

    Delete code directory

--- code/U2SpyPlane.java
+++
@@ -1,8 +0,0 @@
-package SubclassInherits;
-
-//Subclass representing a U-2 Spy Plane
-class U2SpyPlane extends Plane {
-    public U2SpyPlane(String name, double wingspan) {
-        super(name, wingspan);// Call the constructor of the superclass (Plane)
-    }
-}
